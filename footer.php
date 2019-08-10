983685a543430e8233a44e9cf54db266cd002b9c
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-09 21:38:36 -0400

    nav.menu

--- footer.php
+++ footer.php
@@ -1,7 +1,20 @@
 <footer>
 	
 	
-
+	<nav class="menu">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<?php wp_nav_menu(array(
+                        'container' => false,
+                        'menu_class' => '',
+                        'items_wrap' => '<ul class="col-md-12">%3$s</ul>',
+                        'theme_location' => 'menu-footer'
+					 ));    ?>
+				</div>
+			</div>
+		</div>
+	</nav>
 	
 	<div class="copyright">
 		<div class="container">
