fce2c599072760e9ed4a29dd7148aadd1f2a2d74
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-09 21:39:45 -0400

    container

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="container">
+	<div class="row">
+		<section class="main col-md-8">
+			<section class="row single-post">
+				
+			</section>
+		
+			
+		</section>
+
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
