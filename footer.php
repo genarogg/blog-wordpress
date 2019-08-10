2038023d55e770b4eec9be04866fcb2125c8d926
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-09 21:38:23 -0400

    copyright

--- footer.php
+++ footer.php
@@ -3,7 +3,15 @@
 	
 
 	
-
+	<div class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>Sitio creado por Genaro Gonzalez - Genarogg</p>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<?php wp_footer(); ?>
 </footer>
