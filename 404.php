7cd40b3d281dac4eb65291875c05226427b90f24
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-09 21:39:55 -0400

    article

--- 404.php
+++ 404.php
@@ -4,7 +4,18 @@
 	<div class="row">
 		<section class="main col-md-8">
 			<section class="row single-post">
-				
+				<article class="col-sm-12 post">
+					<div class="contenedor">
+						<div class="info">
+							<h2 class="titulo">Error 404</h2>
+							<div class="texto">
+								La página que buscas no se encuentra, por favor verifica que la URL sea correcta.
+								<br>
+								<a href="<?php bloginfo('url'); ?>">Regresar al Inicio</a>
+							</div>
+						</div>
+					</div>
+				</article>
 			</section>
 		
 			
