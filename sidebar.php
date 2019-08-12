2bcd8bee128017036d1a550725a213164034653b
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-11 21:58:08 -0400

    sidebar

---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<aside class="sidebar col-md-4">
+	<div class="widget redes-sociales">
+		<div class="titulo-seccion">
+			<h3>Síguenos</h3>
+		</div>
+		
+	</div>
+
+	
+
+	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : endif; ?>
+</aside>
