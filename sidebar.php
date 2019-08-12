f69717b231a33bd21f4d52dd55611d946446c633
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-11 21:58:24 -0400

    redes-sociales

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,11 @@
 		<div class="titulo-seccion">
 			<h3>Síguenos</h3>
 		</div>
-		
+		<div class="redes-sociales">
+			<a class="youtube" href="http://www.youtube.com/falconmasters"><i class="icono fa fa-youtube-play"></i><span class="seguidores">90K</span></a>
+			<a class="facebook" href="http://www.facebook.com/falconmasters"><i class="icono fa fa-facebook"></i><span class="seguidores">6K</span></a>
+			<a class="twitter" href="http://www.twitter.com/falconmasters"><i class="icono fa fa-twitter"></i><span class="seguidores">3.5K</span></a>
+		</div>
 	</div>
 
 	
