713969830bf3a8d65b9d76421cbd9b2cb75ee171
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-11 22:05:37 -0400

    contenedor

--- index.php
+++ index.php
@@ -29,7 +29,16 @@
 					</div>
 				</article>
 
-				
+				<?php endwhile; else: ?>
+					<article class="col-sm-6 post">
+						<div class="contenedor">
+							<div class="info">
+								<h2 class="titulo">El Post que buscas no existe</h2>
+								<div class="extracto"><p>Revisa que la URL que hayas ingresado sea correcta</p></div>
+							</div>
+						</div>
+					</article>
+				<?php endif; ?>
 			</section>
 		
 			
