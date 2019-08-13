d9b006a573f7cfc9e6cd3e27c27d044e4528d454
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-12 22:09:50 -0400

    paginacion

--- single.php
+++ single.php
@@ -40,7 +40,11 @@
 				<?php endif; ?>
 			</section>
 		
-			
+			<section class="row paginacion">
+				<div class="col-md-12">
+					<div class="pagination"><?php wp_pagenavi(); ?></div>
+				</div>
+			</section>
 		</section>
 
 		<?php get_sidebar(); ?>
