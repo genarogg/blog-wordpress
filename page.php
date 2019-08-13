f67c066ae9e092aa19eab4234892a553ce6ad1e2
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-12 22:07:06 -0400

    contenedor

--- page.php
+++ page.php
@@ -5,7 +5,19 @@
 		<section class="main col-md-8">
 			<section class="row single-post">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					
+					<article class="col-sm-12 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'homepage-thumb' ); } ?>
+							</div>
+							<div class="info">
+								<h2 class="titulo"><?php the_title(); ?></h2>
+								<div class="texto">
+									<?php the_content(); ?>
+								</div>
+							</div>
+						</div>
+					</article>
 				<?php endwhile; else: ?>
 					
 				<?php endif; ?>
