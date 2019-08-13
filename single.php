47152cb1ea2fd91a4a7048129ed37ff39b3ca236
Genaro Gonzalez <genarrogg@gmail.com>
2019-08-12 22:09:24 -0400

    contenedor

--- single.php
+++ single.php
@@ -5,7 +5,28 @@
 		<section class="main col-md-8">
 			<section class="row single-post">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					
+					<article class="col-sm-12 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'homepage-thumb' ); } ?>
+							</div>
+							<div class="info">
+								<h2 class="titulo"><?php the_title(); ?></h2>
+								<p class="fecha"><?php the_date(); ?></p>
+								<div class="categorias">
+									<?php the_category();?>
+								</div>
+								<div class="texto">
+									<?php the_content(); ?>
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<section class="comentarios col-sm-12">
+						<?php comments_template(); ?> 
+					</section>
+			
 			</section>
 		
 			
